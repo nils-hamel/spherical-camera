ed5eb32f8d69d2a68ea59721142bc1277646f392
nils-hamel <nils.hamel@bluewin.ch>
2018-11-30 10:51:09 +0100

    Adding rotation and tranlsation indexation during pose estimation and correction of test script

--- src/algorithm/algorithm_test_random.m
+++ src/algorithm/algorithm_test_random.m
@@ -1,8 +1,8 @@
 
-    function algorithm_test()
+    function algorithm_test_random( sc_count, sc_output )
 
         % create scene %
-        sc_scene = algorithm_scene( zeros(1,3), 10, 20, false );
+        sc_scene = algorithm_scene( zeros(1,3), 10, sc_count, false );
 
         % create camera center %
         sc_camac = ( rand(1,3) - 0.5 ) * 10;
@@ -12,7 +12,7 @@
         sc_camar = algorithm_randrot();
         sc_cambr = algorithm_randrot();
 
-        % compute direction (features) %
+        % compute directions - features %
         sc_camad = algorithm_project( sc_camac, sc_camar, sc_scene );
         sc_cambd = algorithm_project( sc_cambc, sc_cambr, sc_scene );
 
@@ -20,10 +20,10 @@
         [ sc_r, sc_t, sc_ra, sc_rb, sc_err_rad ] = algorithm_pose( sc_camad, sc_cambd, 1e-8 );
 
         % true rotation %
-        sc_r_ = sc_cambr * sc_camar'
+        sc_r_ = sc_cambr * sc_camar';
 
         % true translation %
-        sc_t_ = sc_cambr * ( sc_cambc - sc_camac )'
+        sc_t_ = sc_cambr * ( sc_cambc - sc_camac )';
 
         % compute error on rotation %
         sc_r_err = norm( sc_r * sc_r_' - eye(3) , 'fro' );
@@ -35,6 +35,6 @@
         printf( 'error on rotation : %g and translation : %g\n', sc_r_err, sc_t_err );
 
         % display configuration %
-        %algorithm_plot_scene( sc_camad, sc_ra, sc_cambd, sc_rb, sc_r, sc_t, './test.png' );
+        algorithm_plot_scene( sc_camad, sc_ra, sc_cambd, sc_rb, sc_r, sc_t, sc_output );
 
     end
